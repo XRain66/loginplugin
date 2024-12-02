92a085811c7cfbcc94e5add14af0cdd8c607c40d
XRain66 <2628883576@qq.com>
2024-12-03 01:54:47 +0800

    123

--- src/main/java/com/example/authplugin/AuthPlugin.java
+++ src/main/java/com/example/authplugin/AuthPlugin.java
@@ -5,10 +5,7 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
-
-import java.util.Optional;
 
 @Plugin(
     id = "auth-plugin",
@@ -21,7 +18,6 @@ public class AuthPlugin {
     private final ProxyServer server;
     private final Logger logger;
     private AuthManager authManager;
-    private RegisteredServer loginServer;
 
     @Inject
     public AuthPlugin(ProxyServer server, Logger logger) {
@@ -32,13 +28,6 @@ public class AuthPlugin {
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
         logger.info("Auth Plugin 正在初始化...");
-        
-        Optional<RegisteredServer> loginServerOptional = server.getServer("login");
-        if (!loginServerOptional.isPresent()) {
-            logger.error("找不到登录服务器！请确保配置了名为 'login' 的服务器！");
-            return;
-        }
-        this.loginServer = loginServerOptional.get();
         
         this.authManager = new AuthManager(this, server, logger);
         server.getEventManager().register(this, new AuthListener(this));
@@ -59,9 +48,5 @@ public class AuthPlugin {
 
     public Logger getLogger() {
         return logger;
-    }
-
-    public RegisteredServer getLoginServer() {
-        return loginServer;
     }
 } 
