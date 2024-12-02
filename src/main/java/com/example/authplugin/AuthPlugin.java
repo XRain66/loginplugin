e8fe5639400af676c614c6879c9b3fa802e8406f
XRain66 <2628883576@qq.com>
2024-12-02 23:15:52 +0800

    66

--- src/main/java/com/example/authplugin/AuthPlugin.java
+++ src/main/java/com/example/authplugin/AuthPlugin.java
@@ -4,9 +4,12 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
 import java.util.Optional;
 
 @Plugin(
@@ -19,13 +22,19 @@ import java.util.Optional;
 public class AuthPlugin {
     private final ProxyServer server;
     private final Logger logger;
+    private final Path dataDirectory;
     private AuthManager authManager;
     private RegisteredServer loginServer;
 
     @Inject
-    public AuthPlugin(ProxyServer server, Logger logger) {
+    public AuthPlugin(
+        ProxyServer server, 
+        Logger logger, 
+        @DataDirectory Path dataDirectory
+    ) {
         this.server = server;
         this.logger = logger;
+        this.dataDirectory = dataDirectory;
     }
 
     @Subscribe
@@ -67,5 +76,9 @@ public class AuthPlugin {
 
     public Logger getLogger() {
         return logger;
+    }
+
+    public Path getDataDirectory() {
+        return dataDirectory;
     }
 } 
