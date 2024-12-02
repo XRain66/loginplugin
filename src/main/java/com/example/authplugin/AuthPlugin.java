13d353b4d4657b4e69b1d7fed61625411bfa6257
XRain66 <2628883576@qq.com>
2024-12-02 22:49:51 +0800

    7

--- src/main/java/com/example/authplugin/AuthPlugin.java
+++ src/main/java/com/example/authplugin/AuthPlugin.java
@@ -1,15 +1,12 @@
 package com.example.authplugin;
 
-import com.google.inject.Inject;
+import javax.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
-
-import java.nio.file.Path;
 import java.util.Optional;
 
 @Plugin(
@@ -19,7 +16,7 @@ import java.util.Optional;
     authors = {"XRain666"},
     description = "A simple auth plugin for Velocity"
 )
-public final class AuthPlugin {
+public class AuthPlugin {
     private final ProxyServer server;
     private final Logger logger;
     private AuthManager authManager;
